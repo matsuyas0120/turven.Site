ee6a76281c4cf471aaabe45a47967dd1ce629473
matsuyas0120 <133099218+matsuyas0120@users.noreply.github.com>
2023-06-19 18:16:30 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,86 +1,106 @@
-// 'use strict';
+'use strict';
 
-// {
-//   const open = document.getElementById('open')
-//   const overlay = document.querySelector('.overlay')
-//   const close = document.getElementById('close')
+{
+  const open = document.getElementById('open')
+  const overlay = document.querySelector('.overlay')
+  const close = document.getElementById('close')
 
-//   open.addEventListener('click', () => {
-//     overlay.classList.add('show')
-//   })
+  open.addEventListener('click', () => {
+    overlay.classList.add('show')
+  })
 
-//   close.addEventListener('click', () => {
-//     overlay.classList.remove('show')
-//   })
-// }
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show')
+  })
+}
 
-// {
-//   const targets = document.querySelectorAll('.move');
+{
+  const targets = document.querySelectorAll('.move');
 
-//   function callback(entries, obs) {
-//     console.log(entries);
+  function callback(entries, obs) {
+    console.log(entries);
 
-//     entries.forEach(entry => {
-//       if (!entry.isIntersecting) {
-//         return;
-//       }
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
   
-//       entry.target.classList.add('appear');
-//       obs.unobserve(entry.target);
-//     });
-//   }
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
 
-//   const options = {
-//     threshold: 0.2,
-//     rootMargin:'0px 0px -100px',
-//   };
+  const options = {
+    threshold: 0.2,
+    rootMargin:'0px 0px -100px',
+  };
 
-//   const observer = new IntersectionObserver(callback, options);
+  const observer = new IntersectionObserver(callback, options);
 
-//   targets.forEach(target => {
-//     observer.observe(target);
-//   });
-// }
+  targets.forEach(target => {
+    observer.observe(target);
+  });
+}
 
-// {
-//   const targets = document.querySelectorAll('.shift');
+{
+  const targets = document.querySelectorAll('.shift');
 
-//   // ②
-//   function callback(entries, obs) {
-//     console.log(entries);
+  // ②
+  function callback(entries, obs) {
+    console.log(entries);
 
-//     entries.forEach(entry => {
-//       if(!entry.isIntersecting) {
-//         return;
-//       }
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        return;
+      }
 
-//       entry.target.classList.add('active');
+      entry.target.classList.add('active');
 
-//       obs.unobserve(entry.target)
-//     });
-//   }
+      obs.unobserve(entry.target)
+    });
+  }
 
-//   const options = {
-//     threshold: 0.2,
-//     rootMargin:'0px 0px -100px',
-//   }
+  const options = {
+    threshold: 0.2,
+    rootMargin:'0px 0px -100px',
+  }
 
-//   // インスタンス①
-//   const observer = new IntersectionObserver(callback, options);
+  // インスタンス①
+  const observer = new IntersectionObserver(callback, options);
 
-//   targets.forEach(target => {
-//     observer.observe(target);
-//   })
-// }
+  targets.forEach(target => {
+    observer.observe(target);
+  })
+}
 
-// // 画像切り替え
+// 画像切り替え
 
-//   {
-//     const thumbnails = document.querySelectorAll('.thumbnail');
-//     const mainImage = document.getElementById('main-img');
+  {
+    const thumbnails = document.querySelectorAll('.thumbnail');
+    const largeImgs = document.querySelectorAll('.large-img');
   
-//     thumbnails[1].addEventListener('click', () => {
-//       thumbnails[0].classList.add('active');
-//       thumbnails[1].classList.remove('active');
-//     });
-//   }
+    thumbnails[0].addEventListener('click', () => {
+      largeImgs[0].classList.add('active');
+      largeImgs[1].classList.remove('active');
+      largeImgs[2].classList.remove('active');
+      largeImgs[3].classList.remove('active');
+    });
+    thumbnails[1].addEventListener('click', () => {
+      largeImgs[1].classList.add('active');
+      largeImgs[0].classList.remove('active');
+      largeImgs[2].classList.remove('active');
+      largeImgs[3].classList.remove('active');
+    });
+    thumbnails[2].addEventListener('click', () => {
+      largeImgs[2].classList.add('active');
+      largeImgs[1].classList.remove('active');
+      largeImgs[0].classList.remove('active');
+      largeImgs[3].classList.remove('active');
+    });
+    thumbnails[3].addEventListener('click', () => {
+      largeImgs[3].classList.add('active');
+      largeImgs[1].classList.remove('active');
+      largeImgs[2].classList.remove('active');
+      largeImgs[0].classList.remove('active');
+    });
+  }
